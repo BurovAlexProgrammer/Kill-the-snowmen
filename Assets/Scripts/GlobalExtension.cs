d9380a453ee7d9c007a03c2aba4f1195c913f981
Aleksey <burovalexprogrammer@gmail.com>
2020-07-09 16:17:55 +0500

    Added my extension
    Added functions to work with hierarchy

---
+++ Assets/Scripts/GlobalExtension.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public static class GlobalExtension
+{
+    /// <summary>
+    /// Write message to console.
+    /// </summary>
+    /// <param name="message">Message</param>
+    public static void log(string message)
+    {
+        Debug.Log(message);
+    }
+
+
+    /// <summary>
+    /// Return first child Transform with tag from current scene
+    /// </summary>
+    /// <param name="parent"></param>
+    /// <param name="tag"></param>
+    /// <returns>child Transform</returns>
+    public static Transform FindChildByTag(this Scene scene, string tag)
+    {
+        foreach (var parent in scene.GetRootGameObjects())
+        {
+            var child = parent.transform.FindChildByTag(tag);
+            if (child != null) { return child; }
+        }
+        return null;
+    }
+
+    /// <summary>
+    /// Return first child Transform with tag in parent
+    /// </summary>
+    /// <param name="parent"></param>
+    /// <param name="tag"></param>
+    /// <returns>child Transform</returns>
+    public static Transform FindChildByTag(this Transform parent, string tag)
+    {
+        var children = parent.transform.Children();
+        return children.Where(c => c.CompareTag(tag)).DefaultIfEmpty(null).FirstOrDefault();
+    }
+
+    /// <summary>
+    /// Return children GameObject list from all parent hierarchy
+    /// </summary>
+    /// <param name="parent"></param>
+    /// <param name="tag"></param>
+    /// <returns></returns>
+    public static List<Transform> ChildrenList(this Transform parent)
+    {
+        var result = new List<Transform>();
+        foreach (Transform transform in parent)
+        {
+            result.Add(transform);
+            if (transform.childCount > 0)
+                result.AddRange(transform.ChildrenList());
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// Return children GameObject as IEnumerable from all parent hierarchy
+    /// </summary>
+    /// <param name="parent"></param>
+    /// <param name="tag"></param>
+    /// <returns>Children IEnumerable of Transform</returns>
+    public static IEnumerable<Transform> Children(this Transform parent)
+    {
+        foreach (Transform transform in parent)
+        {
+            yield return transform;
+            if (transform.childCount > 0)
+                foreach (var item in transform.Children())
+                    yield return item;
+        }
+    }
+
+
+    /// <summary>
+    /// Return children Transform with tag. 
+    /// </summary>
+    /// <param name="parent"></param>
+    /// <param name="tag"></param>
+    /// <param name="onlyActive">Take only active</param>
+    /// <returns>List of Transform</returns>
+    public static List<Transform> FindChildrenByTag(this GameObject parent, string tag, bool onlyActive = false)
+    {
+        var children = parent.transform.Children();
+        if (onlyActive)
+            return children.Where(c => c.CompareTag(tag) & c.gameObject.activeSelf).ToList();
+        else
+            return children.Where(c => c.CompareTag(tag)).ToList();
+    }
+
+    /// <summary>
+    /// Check is null or destroyed current object and return bool.
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns>bool</returns>
+    public static bool NotExist(this object obj)
+    {
+        if (obj is IComponent)
+            return (obj as IComponent).isDestroyed;
+        else if (obj is Component)
+            return (obj as Component) == null;
+        else
+            return obj == null;
+    }
+
+    /// <summary>
+    /// Return parent GameObject with tag.
+    /// </summary>
+    /// <param name="childObject"></param>
+    /// <param name="tag"></param>
+    /// <returns>parent GameObject or null on failed.</returns>
+    public static GameObject FindParentByTag(this GameObject childObject, string tag)
+    {
+        Transform t = childObject.transform;
+        while (t.parent != null)
+        {
+            if (t.parent.tag == tag)
+            {
+                return t.parent.gameObject;
+            }
+            t = t.parent.transform;
+        }
+        return null;
+    }
+
+    interface IComponent
+    {
+        GameObject gameObject { get; }
+        Transform transform { get; }
+        Component component { get; }
+        bool isDestroyed { get; }
+    }
+
+    public static class Effects
+    {
+        //public static void FadeInScene(this Camera camera)
+        //{
+        //    var frame = camera.gameObject.FindChildByTag(Tags.CAMERA_EFFECT);
+        //    frame.
+        //}
+    }
+
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static GlobalExtension;
+
+public class InputController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            var scene = SceneManager.GetActiveScene();
+            //var gs = scene.GetRootGameObjects();
+            //var g2 = gs.Where(g => g.CompareTag(Tags.CAMERA_EFFECT));
+            //g2?.First()?.GetComponent<Animator>().SetBool("FadeOut", true);
+            //gameObject.Tag
+            //var sr = scene.FindChildByTag(Tags.CAMERA_EFFECT);
+            //sr.GetComponent<Animator>().SetBool("FadeOut", true);
+            var list = new List<int>() { 1,2,34,5,7};
+            var re = list.Where(l => l == 0)?.FirstOrDefault();
+
+            var c = Camera.main.transform.Children();
+            var c2 = Camera.main.transform.Children().ToList();
+        }
+    }
+
+    
+}
